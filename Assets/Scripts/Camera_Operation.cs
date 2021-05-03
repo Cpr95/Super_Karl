d56d477c3767f58f33fc44383964cb63d994c312
Cpr95 <74544949+Cpr95@users.noreply.github.com>
2021-05-03 12:31:44 +0200

    Initial commit

---
+++ Assets/Scripts/Camera_Operation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Operation : MonoBehaviour
+{
+    private GameObject player;
+    public float xMin; //these variables will clamp the camera into a certain position
+    public float xMax;
+    public float yMin;
+    public float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player"); // this searches the game and finds any objects tagged player and connects it with GameObject
+    }
+
+    // Update is called once per frame
+    void LateUpdate() // Lateupdate is better when using camera systems than using update
+    {
+        float x = Mathf.Clamp (player.transform.position.x, xMin, xMax);
+        float y = Mathf.Clamp (player.transform.position.y, yMin, yMax);
+        gameObject.transform.position = new Vector3 (x, y, gameObject.transform.position.z); 
+    }
+}
---
+++ Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public int playerSpeed = 10; // This is for the characters speed
+    public bool facingRight = true; // true or false, is the player facing left or right ?
+    public int playerJump = 1250;
+    private float moveX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove (); // so that the player is able to move every single frame 
+    }
+
+    void PlayerMove() 
+    {
+
+        // Player Controls 
+        moveX = Input.GetAxis("Horizontal"); //the player can move on the horizontal axis
+
+        if (Input.GetButtonDown ("Jump")) //if user presses jump button player will jump
+        {
+            Jump();
+        }
+
+        // Player Animations
+
+        // Player Direction
+        if (moveX < 0.0f && facingRight == false)  // if player is not facing right, the player will now face to the left
+        {
+            FlipPlayer ();
+        }
+        else if (moveX > 0.0f && facingRight == true)
+        {
+            FlipPlayer ();
+        }
+
+        // Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2 (moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+        // this will make the player move left and right
+    }
+    
+    void Jump()
+    {
+        //Player Jump Code
+        GetComponent<Rigidbody2D>().AddForce (Vector2.up * playerJump); // this will add force upwards associated to the player 
+    }
+
+    void FlipPlayer() 
+    {
+        facingRight = !facingRight; // the exclamation mark means not true - facing right does not equal facing right
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale; 
+        // if player is facing right and does not want to face right any more, local scale will change the scale to negative
+    }
+}
