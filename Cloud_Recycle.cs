e52ab788dd56ff27d715681873f387a44b2bbbac
Cpr95 <74544949+Cpr95@users.noreply.github.com>
2021-05-04 19:07:24 +0200

    Player Health + Cloud Recycle + Enemy + Background
    player starts again when he falls, clouds recycle but still need some tweaks, Enemy moves from left to right and background is clipped with camera

---
+++ Cloud_Recycle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud_Recycle : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 1f;
+    [SerializeField]
+    private bool _randomizeHeight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3.left = Vector3(-1,0,0)
+        //Vector3.left (-1) * 1 * _speed(3) = -3
+        transform.Translate(Vector3.left * Time.deltaTime * _speed);
+
+        //check the x position of the seaweed if it is smaller then -9 
+        if(transform.position.x < -20)
+        {
+            if(_randomizeHeight)
+            {
+                float randomYPosition = Random.Range(0f,4f);
+                Debug.Log("The random position is: " + randomYPosition);
+                //teleport to 13 on the x axis
+                transform.position = new Vector3(15,randomYPosition,0);
+            }
+            else
+            {
+                transform.position = new Vector3(20,transform.position.y,transform.position.z);
+            }
+        }
+        
+    }
+}
---
+++ Enemy.cs
@@ -0,0 +1,52 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int enemySpeed;
+    public int MoveDirection;
+    public bool moveRight = true;
+
+ // Use this for initialization
+ void Start () {
+        moveRight = true;
+ }
+ 
+ // Update is called once per frame
+ void Update () {
+        if (!moveRight)
+        {
+            MoveDirection = 1;
+        }
+        else
+        {
+            MoveDirection = -1;
+        }
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(MoveDirection, 0) * enemySpeed;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            if(MoveDirection == -1)
+            {
+                Flip ();
+            }
+            else
+            {
+                Flip ();
+            }
+        }
+    }
+
+    void Flip () 
+    {
+        moveRight = !moveRight; // the exclamation mark means not true - facing right does not equal facing right
+        Vector2 localScale = gameObject.transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale; 
+        // if player is facing right and does not want to face right any more, local scale will change the scale to negative
+    }
+}
