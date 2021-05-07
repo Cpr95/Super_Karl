8060a81424440b4f18a67606f145d3a889ad0a37
Renee xuereb <xuereb.renee@gmail.com>
2021-05-07 14:31:17 +0200

    Added Score and Time Left

--- Assets/Scripts/Player_Score.cs
+++ Assets/Scripts/Player_Score.cs
@@ -4,41 +4,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-
 public class Player_Score : MonoBehaviour
 {
+
     private float timeLeft = 120;
     public int playerScore = 0;
     public GameObject timeLeftUI;
     public GameObject playerScoreUI;
 
-    void Update() 
+    
+    
+    // Update is called once per frame
+    void Update()
     {
-        timeLeft -= Time.deltaTime;
-        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)timeLeft); 
-        if (timeLeft < 0.1)
+        timeLeft -= Time.deltaTime; 
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)timeLeft);
+        playerScoreUI.gameObject.GetComponent<Text>().text = ("Score: " + playerScore); 
+        if (timeLeft < 0.1f)
         {
-            SceneManager.LoadScene ("0");
+            SceneManager.LoadScene (0);
         }
-        
+    } 
     void OnTriggerEnter2D (Collider2D trig)
     {
-        if (trig.gameObject.name == "EndLevel")
-        {
+        Debug.Log ("Touched the end of the level");
         CountScore ();
-        }
-        if (trig.gameObject.name == "Coin")
-        {
-        playerScore += 100;
-        Destroy (trig.gameObject);
-        }
     }
     void CountScore ()
     {
-        playerScore =  playerScore + (int)(timeLeft * 10);
+        playerScore = playerScore + (int)(timeLeft * 10);
         Debug.Log (playerScore);
     }
-    }
-
+    
 }
-
