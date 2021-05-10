3ec80481b8f0f15f18940596c6c84928042d63b6
Renee xuereb <xuereb.renee@gmail.com>
2021-05-10 14:44:00 +0200

    Created DataManagement Script

--- Assets/Scripts/Player_Score.cs
+++ Assets/Scripts/Player_Score.cs
@@ -6,26 +6,44 @@ using UnityEngine.UI;
 
 public class Player_Score : MonoBehaviour
 {
-
-    public int playerScore = 0;
-    public GameObject timeLeftUI;
-    public GameObject playerScoreUI;
+private float timeLeft = 120;
+public int playerScore = 0;
+public GameObject timeLeftUI;
+public GameObject playerScoreUI;   
 
     
     
     // Update is called once per frame
     void Update()
-    {
+    { 
+        timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<Text>().text = ("Time Left: " + (int)timeLeft);
         playerScoreUI.gameObject.GetComponent<Text>().text = ("Score: " + playerScore); 
+        if (timeLeft <0.1f) 
+        {
+            SceneManager.LoadScene ("0");
+        }
     } 
     void OnTriggerEnter2D (Collider2D trig)
     { 
+        if (trig.gameObject.name == "EndLevel")
+        {
+            CountScore (); 
+        }
         if (trig.gameObject.name == "Coin")
         {
             playerScore += 100;
             Destroy (trig.gameObject);
         }
         
+    }
+    void CountScore ()
+    {
+        Debug.Log ("Data says high score is currently" + DataManagement.datamanagement.highScore);
+        playerScore = playerScore + (int)(timeLeft * 10);
+        DataManagement.datamanagement.highScore = playerScore + (int)(timeLeft * 10);
+        DataManagement.datamanagement.SaveData ();
+        Debug.Log ("Now that we have adde the score toDataManagement, Data says high score is currently" + DataManagement.datamanagement.highScore);
     }
     
 }
