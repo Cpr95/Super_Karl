bfcb1ef44c4ea03098ce6cd83c0b3e9582044fac
Cpr95 <74544949+Cpr95@users.noreply.github.com>
2021-05-10 09:42:24 +0200

    changed enemy code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,33 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Enemy : MonoBehaviour
-{
-    public int EnemySpeed;
-    public int XMoveDirection;
-
-    // Update is called once per frame
-    void Update()
-    {
-        RaycastHit2D hit = Physics2D.Raycast (transform.position, new Vector2 (XMoveDirection, 0));
-        gameObject.GetComponent<Rigidbody2D> ().velocity = new Vector2 (XMoveDirection,0) * EnemySpeed;
-        if (hit.distance < 0.7f)
-        {
-            Flip();
-        }
-    }
-
-    void Flip ()
-    {
-        if(XMoveDirection > 0)
-        {
-            XMoveDirection = -1;
-        }
-        else 
-        {
-            XMoveDirection = 1;
-        }
-    }
-    
-}
+﻿ using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class EnemyPatrol : MonoBehaviour
+ {
+     public float speed;
+     public float distance;
+ 
+     private bool movingRight = true;
+ 
+     public Transform groundDetection;
+ 
+     void Update()
+     {
+         transform.Translate(Vector2.right * speed * Time.deltaTime);
+ 
+         RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
+         if (groundInfo.collider == false)
+         {
+             if (movingRight == true)
+             {
+                 transform.eulerAngles = new Vector3(0, -180, 0);
+                 movingRight = false;
+             }
+             else
+             {
+                 transform.eulerAngles = new Vector3(0, 0, 0);
+                 movingRight = true;
+             }
+           
+         }
+            void OnCollisionEnter2D(Collision2D collision);
+            
+     if (collide.collider.gameObject.tag == "Wall")
+         {
+             Debug.Log("E");
+             if (movingRight == true)
+             {
+                 transform.eulerAngles = new Vector3(0, -180, 0);
+                 movingRight = false;
+             }
+             else
+             {
+                 transform.eulerAngles = new Vector3(0, 0, 0);
+                 movingRight = true;
+             }
+ 
+         }
+     }
+ }
